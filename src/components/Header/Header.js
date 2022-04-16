0a6f1d68570f7e8025b16a34e887013710ead912
Rajib Das <rajibrad@gmail.com>
2022-04-16 15:12:57 +0600

    create nav bar and little css on it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
     return (
         <div className='Header'>
             <img src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Riview</a>
+                <a href="/manage">Manage Invertory</a>
+            </nav>
+
         </div>
     );
 };
